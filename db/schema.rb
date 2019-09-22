9461a947b476f1c2935ad98494d5b6c166f480d7
ASHOK MAHARJAN <ashok997@gmail.com>
2019-09-22 20:13:56 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,4 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "students", force: :cascade do |t|
-    t.string   "fisrt_name"
-    t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
